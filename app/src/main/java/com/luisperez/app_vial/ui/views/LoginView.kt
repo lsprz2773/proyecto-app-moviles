7f07b0d7fa8dbc00263c6b1d854e4da3fa46f0eb
Emma <reyespreciadoemma@gmail.com>
2025-11-24 13:50:35 -0600

    Inicio de LoginView

--- app/src/main/java/com/luisperez/app_vial/ui/views/LoginView.kt
+++ app/src/main/java/com/luisperez/app_vial/ui/views/LoginView.kt
@@ -1,2 +1,14 @@
 package com.luisperez.app_vial.ui.views
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoginView(){
+
+}
+
+@Composable
+fun LoginContent(){
+
+}
+
--- app/src/main/java/com/luisperez/app_vial/ui/views/RegisterView.kt
+++ app/src/main/java/com/luisperez/app_vial/ui/views/RegisterView.kt
@@ -1,11 +1,5 @@
 package com.luisperez.app_vial.ui.views
 
-import android.R
-import android.annotation.SuppressLint
-import android.text.style.BackgroundColorSpan
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -14,22 +8,16 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -38,10 +26,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
